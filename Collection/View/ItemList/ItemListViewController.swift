81e2e8435babfaa2684041a4983003cd9c41e0f9
Hanna Chen <hanna.yhchen@gmail.com>
2022-10-28 18:26:10 +0800

    Setup group structure

---
+++ Collection/View/ItemList/ItemListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItemListViewController.swift
+//  Collection
+//
+//  Created by Hanna Chen on 2022/10/28.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
