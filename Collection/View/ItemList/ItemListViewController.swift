d187e4d8b8150794fb23ab1af74927e6720bd9c9
Hanna Chen <hanna.yhchen@gmail.com>
2022-11-20 20:36:08 +0800

    Add custom height of sheet for iOS 16

--- Collection/View/ItemList/ItemListViewController.swift
+++ Collection/View/ItemList/ItemListViewController.swift
@@ -152,7 +152,11 @@ class ItemListViewController: UIViewController {
         importController.modalPresentationStyle = .formSheet
         importController.preferredContentSize = CGSize(width: 300, height: 400)
         if let sheet = importController.sheetPresentationController {
-            sheet.detents = [.medium()]
+            if #available(iOS 16.0, *) {
+                sheet.detents = [.custom { _ in 240 }]
+            } else {
+                sheet.detents = [.medium()]
+            }
             sheet.prefersScrollingExpandsWhenScrolledToEdge = false
             sheet.prefersEdgeAttachedInCompactHeight = true
             sheet.preferredCornerRadius = 30
